7ab6be2743428317711330277ef4e0a6339f9f6b
IsopeTeam <57916042+IsopeTeam@users.noreply.github.com>
2021-02-04 16:16:45 +0100

    Update interface graphique.py

--- interface graphique.py
+++ interface graphique.py
@@ -260,8 +260,7 @@ def window_trial_update(average):
 
 
 def window_tag_list_maker(location=(650,0)):
-    #os.chdir(os.path.dirname(path_dic['list_condition_path_ephy'][0]))
-    os.chdir(os.path.dirname('C:\\Users\\Master5.INCI-NSN\\Desktop\\Pierre\\data'))
+    os.chdir(os.path.dirname(path_dic['list_condition_path_ephy'][0]))
     
     treedata = sg.TreeData()
     if os.path.exists('taged_trials.txt'):
@@ -577,8 +576,8 @@ while True:
         if event=='tag':
             if values['trial_by_trial']:
                 if 'choice' in globals():
-                    #os.chdir(os.path.dirname(path_dic['list_condition_path_ephy'][0]))
-                    os.chdir(os.path.dirname('C:\\Users\\Master5.INCI-NSN\\Desktop\\Pierre\\data'))
+                    os.chdir(os.path.dirname(path_dic['list_condition_path_ephy'][0]))
+                    
                     if os.path.exists('taged_trials.txt'):
                         with open ('taged_trials.txt', 'rb') as tag_trials_file:
                             my_depickler = pickle.Unpickler(tag_trials_file)
@@ -607,8 +606,8 @@ while True:
         if event=='untag':
             if values['trial_by_trial']:
                 if 'choice' in globals():
-                    #os.chdir(os.path.dirname(path_dic['list_condition_path_ephy'][0]))
-                    os.chdir(os.path.dirname('C:\\Users\\Master5.INCI-NSN\\Desktop\\Pierre\\data'))
+                    os.chdir(os.path.dirname(path_dic['list_condition_path_ephy'][0]))
+                    
                     if os.path.exists('taged_trials.txt'):
                         with open ('taged_trials.txt', 'rb') as tag_trials_file:
                             my_depickler = pickle.Unpickler(tag_trials_file)
